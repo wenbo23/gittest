45fd6a4a59b8fb9387a506949c6a9474ba50c17a
wenbo <ywenbo@gmail.com>
2024-08-14 22:57:37 +0800

    modify run.bat script

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import sys
+
+def func1():
+    print("Function 1 is executed")
+
+def func2():
+    print("Function 2 is executed")
+
 def main():
-    print("Hello, World!")
+    args = sys.argv[1:]  # 第一个参数是脚本名称，实际参数从sys.argv[1:]开始
+    print("Received arguments:", args)
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
+        if command == 'func1':
+            func1()
+        elif command == 'func2':
+            func2()
+        else:
+            print("Unknown command")
+    else:
+        print("No command provided")
 
 if __name__ == "__main__":
     main()
